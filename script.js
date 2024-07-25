162581caece197b0bea4a0120e27889748cfc8f2
JimHuynh25 <112700814+JimHuynh25@users.noreply.github.com>
2024-07-24 19:59:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+
+    const sectionToShow = document.getElementById(sectionId);
+    sectionToShow.classList.add('active');
+}
